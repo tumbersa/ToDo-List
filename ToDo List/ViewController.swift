dfd75e081a974a5a228cfc970928a89520d94ba7
Глеб Капустин <gleb.kapustin.98@mail.ru>
2025-03-13 20:34:40 +0300

    Initial Commit

---
+++ ToDo List/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ToDo List
+//
+//  Created by Глеб Капустин on 13.03.2025.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
